187dbb62bd9582fc25f0f3231092ea615261cc9e
Bob Jackson <wangyousong@9191offer.com>
2024-10-01 18:17:57 +0800

    实施 react-dnd 重构以优化列组件的拖放功能
    - 引入 useDrop 和 useItemDrag 钩子以处理拖放逻辑
    - 优化拖放操作，使用 throttle 函数限制 hover 事件的触发频率
    -调整列移动逻辑，提高交互的准确性和流畅度
    - 删除了 setDraggedItem 函数的多余代码

--- src/Column.tsx
+++ src/Column.tsx
@@ -1,9 +1,12 @@
 import {ColumnContainer, ColumnTitle} from "./styles.ts";
-import React from "react";
+import React, {useRef} from "react";
 import {Card} from "./Card.tsx";
 import {AddNewItem} from "./AddNewItem.tsx";
 import {useAppState} from "./state/AppStateContext.tsx";
-import {addTask} from "./state/actions.ts";
+import {addTask, moveList} from "./state/actions.ts";
+import {useDrop} from "react-dnd";
+import {throttle} from "throttle-debounce-ts";
+import {useItemDrag} from "./utils/useItemDrag.ts";
 
 type ColumnProps = {
     text: string
@@ -12,12 +15,33 @@ type ColumnProps = {
 }
 
 export const Column = ({text, id}: ColumnProps) => {
-    const {getTasksByListId, dispatch} = useAppState()
+    const {draggedItem, getTasksByListId, dispatch} = useAppState()
 
     const tasks = getTasksByListId(id)
 
+
+    const ref = useRef<HTMLDivElement>(null)
+    const [, drop] = useDrop({
+        accept: "COLUMN",
+        hover: throttle(200, () => {
+            if (!draggedItem) {
+                return
+            }
+            if (draggedItem.type === "COLUMN") {
+                if (draggedItem.id === id) {
+                    return
+                }
+                dispatch(moveList(draggedItem.id, id))
+            }
+        })
+    })
+
+    const {drag} = useItemDrag({type: "COLUMN", id, text})
+
+    drag(drop(ref))
+
     return (
-        <ColumnContainer>
+        <ColumnContainer ref={ref}>
             <ColumnTitle>{text}</ColumnTitle>
             {tasks.map(task => (
                 <Card key={task.id} text={task.text} id={task.id}/>
