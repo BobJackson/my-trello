553f95362ab113fc2128f19d25a0d906c012fa3f
Bob Jackson <wangyousong@9191offer.com>
2024-10-01 13:59:53 +0800

    新增添加新项功能组件
    - 添加 AddNewItem 组件，用于在列表中添加新项
    - 添加 NewItemForm 组件，用于提供添加新项的表单
    - 更新 Column 组件，集成添加新卡片的功能
    - 在 App 组件中添加示例用的 AddNewItem 组件
    - 更新样式文件，添加新组件的样式定义

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {AppContainer} from "./styles"
 import {Column} from "./Column.tsx";
+import {AddNewItem} from "./AddNewItem.tsx";
 
 function App() {
   return (
     <AppContainer>
         <Column text="Todo:"/>
+        <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
     </AppContainer>
   )
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -1,6 +1,7 @@
 import {ColumnContainer, ColumnTitle} from "./styles.ts";
 import React from "react";
 import {Card} from "./Card.tsx";
+import {AddNewItem} from "./AddNewItem.tsx";
 
 type ColumnProps = {
     text: string
@@ -14,6 +15,7 @@ export const Column = ({text}: ColumnProps) => {
             <Card text="Generate app scaffold"/>
             <Card text="Learn TypeScript"/>
             <Card text="Third Item"/>
+            <AddNewItem onAdd={console.log} toggleButtonText="+ Add another card" dark/>
         </ColumnContainer>
     )
 }
