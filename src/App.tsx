175a4faa74d2b80260e06fd743af67e36647289c
Bob Jackson <wangyousong@9191offer.com>
2024-10-01 14:39:41 +0800

    实施全局状态管理并优化组件- 引入 AppStateContext 提供全局状态管理，优化组件间数据交互 - 重构 App 组件，动态渲染列表和任务 - 更新 Column 组件，通过全局状态获取任务列表 - 修改 Card组件，增加任务 ID 属性- 在 main.tsx 中引入 AppStateProvider 以提供全局状态

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import {AppContainer} from "./styles"
 import {Column} from "./Column.tsx";
 import {AddNewItem} from "./AddNewItem.tsx";
+import {useAppState} from "./state/AppStateContext.tsx";
 
 function App() {
+    const {lists} = useAppState()
+
   return (
-    <AppContainer>
-        <Column text="Todo:"/>
-        <AddNewItem toggleButtonText="+ Add another list" onAdd={console.log}/>
+      <AppContainer>
+          {lists.map(list => (
+              <Column text={list.text} key={list.id} id={list.id}/>
+          ))}
+          <AddNewItem
+              toggleButtonText="+ Add another list"
+              onAdd={console.log}
+          />
     </AppContainer>
   )
 }
--- src/Column.tsx
+++ src/Column.tsx
@@ -2,20 +2,29 @@ import {ColumnContainer, ColumnTitle} from "./styles.ts";
 import React from "react";
 import {Card} from "./Card.tsx";
 import {AddNewItem} from "./AddNewItem.tsx";
+import {useAppState} from "./state/AppStateContext.tsx";
 
 type ColumnProps = {
     text: string
+    id: string
     children?: React.ReactNode
 }
 
-export const Column = ({text}: ColumnProps) => {
+export const Column = ({text, id}: ColumnProps) => {
+    const {getTasksByListId} = useAppState()
+
+    const tasks = getTasksByListId(id)
+
     return (
         <ColumnContainer>
             <ColumnTitle>{text}</ColumnTitle>
-            <Card text="Generate app scaffold"/>
-            <Card text="Learn TypeScript"/>
-            <Card text="Third Item"/>
-            <AddNewItem onAdd={console.log} toggleButtonText="+ Add another card" dark/>
+            {tasks.map(task => (
+                <Card key={task.id} text={task.text} id={task.id}/>
+            ))}
+            <AddNewItem
+                onAdd={console.log}
+                toggleButtonText="+ Add another card"
+                dark/>
         </ColumnContainer>
     )
 }
