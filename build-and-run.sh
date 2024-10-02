7fd18c0d038d821da67c83217a56d941e975bc15
Bob Jackson <wangyousong@9191offer.com>
2024-10-02 16:51:02 +0800

    确保TrelloUI容器在宿主机重启后自动启动
    在`build-and-run.sh`脚本中添加了`--restart always`参数，使TrelloUI的Docker容器在宿主机器重启后能够自动启动，保证服务的持续运行。

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,4 +2,4 @@ git pull
 docker stop trello-ui
 docker rm trello-ui
 docker build -t trello-ui .
-docker run -d -p 10083:80 --name trello-ui --network www_wordpress-network trello-ui
+docker run -d -p 10083:80 --restart always --name trello-ui --network www_wordpress-network trello-ui
